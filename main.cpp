40b72afc107ef6e580944e330f738c940038a051
AmirrezaEghdami <eghdamiamirreza@gmail.com>
2022-08-05 19:40:57 +0430

    :-)

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+
+	string firstS;
+	string secondS;
+    cin >> firstS;
+    cin >> secondS;
+
+	if (firstS[0] == secondS[secondS.length()-1])
+	{
+		cout << "YES";
+		return 1;
+	}
+
+	cout << "NO";
+	return 0;
+}
